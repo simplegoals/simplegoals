4284c1217141a92628389efdfbbe753f92f2e8f6
Slava Korolev <korolvs@gmail.com>
2019-09-15 13:56:36 +0200

    Added mobile support

--- draft/index.js
+++ draft/index.js
@@ -2,16 +2,17 @@ const SimpleGoals = (() => {
   class Goals {
   }
 
+  let achievement = null
+  let achievementTimeout = null
+
   const init = config => {
+    achievement = document.getElementById('simplegoals-achievement')
+    achievement.addEventListener('click', event => achievement.classList.toggle('simplegoals-achievement--clicked'))
     const trigger = document.getElementById('simplegoals-achievement__close-button')
     trigger.addEventListener('click', event => hide())
   }
 
-  let achievement = null
-  let achievementTimeout = null
-
   const show = config => {
-    achievement = document.getElementById('simplegoals-achievement')
     achievement.classList.remove('simplegoals-achievement--closed')
     achievement.classList.add('simplegoals-achievement--opened')
     clearTimeout(achievementTimeout)
