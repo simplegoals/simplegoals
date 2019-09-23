7eed3afffd9c3b29591a3a9aa76004bb10851707
Slava Korolev <korolvs@gmail.com>
2019-09-23 23:01:17 +0200

    Added overview generaing code

--- draft/index.js
+++ draft/index.js
@@ -19,8 +19,15 @@ const SimpleGoals = (() => {
     }
   }
 
-  const createAchievementNotificationHtml = () => {
-    const achievementNotificationHtmlString = `
+  const prepareGoals = config => {
+    goals = config.goals
+    for (const key of Object.keys(goals)) {
+      goals[key].unlocked = true
+    }
+  }
+
+  const createAchievementHtml = () => {
+    const achievementHtmlString = `
     <div class="simplegoals-achievement" id="simplegoals-achievement">
       <div class="simplegoals-achievement__icon">
         <img src="cup.svg" />
@@ -42,12 +49,57 @@ const SimpleGoals = (() => {
       </button>
     </div>
     `
-    const node = new DOMParser().parseFromString(achievementNotificationHtmlString , 'text/html').body.firstChild
+    const node = new DOMParser().parseFromString(achievementHtmlString , 'text/html').body.firstChild
+    document.body.appendChild(node)
+  }
+
+  const overviewGoalsHtml = () => {
+    let result = "";
+    for (const key of Object.keys(goals)) {
+      result += `
+      <div class="simplegoals-overview-goal ${goals[key].unlocked ? "" : "simplegoals-overview-goal--locked"}">
+        <div class="simplegoals-overview-goal__icon">
+          <img src="cup.svg" />
+        </div>
+        <div class="simplegoals-overview-goal__body">
+          <h3 class="simplegoals-overview-goal__name">${goals[key].name}</h3>
+          <p class="simplegoals-overview-goal__description">
+            ${goals[key].descripton}
+          </p>
+        </div>
+      </div>
+      `
+    }
+    return result
+  }
+
+  const createOverviewHtml = () => {
+    const overviewHtmlString = `
+    <div class="simplegoals-overview" id="simplegoals-overview">
+      <div class="simplegoals-overview__wrapper">
+        <div class="simplegoals-overview__body">
+          <h1 class="simplegoals-overview__title">Achievements</h1>
+          <p class="simplegoals-overview__subtitle">Complete tasks and unlock achievements</p>
+          <div class="simplegoals-overview__goals">
+          ` + overviewGoalsHtml() + `
+          </div>
+          <button class="simplegoals-overview__close-button" id="simplegoals-overview__close-button">
+            Close
+          </button>
+          <div class="simplegoals-overview__attribution">
+            Powered by <a class="simplegoals-overview__attribution-link" href="https://simplegoals.co" target="_blank">SimpleGoals</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    `
+    const node = new DOMParser().parseFromString(overviewHtmlString , 'text/html').body.firstChild
     document.body.appendChild(node)
   }
 
   const createDOMElements = () => {
-    createAchievementNotificationHtml()
+    createOverviewHtml()
+    createAchievementHtml()
   }
 
   const setGoal = (newGoal) => {
@@ -102,7 +154,7 @@ const SimpleGoals = (() => {
 
   const init = config => {
     setOptions(config)
-    goals = config.goals
+    prepareGoals(config)
     createDOMElements()
     initAchievement()
     initOverview()
